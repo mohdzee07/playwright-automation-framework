543bffe5d902122c5fa04a4df9e99eecb7cb718a
Zeeshan <mohdzee1414@gmail.com>
2026-08-22 01:26:55 +0530

    ENV DOT PUSH

--- playwright.config.js
+++ playwright.config.js
@@ -5,9 +5,13 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 // import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config(
+  { 
+    //path:`./env/.env.${process.env.TEST_ENV}` 
+    path:process.env.TEST_ENV ? `./env/.env.${process.env.TEST_ENV}` : `./env/.env.dev`
+  });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -32,6 +36,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless : false,
   },
 
   /* Configure projects for major browsers */
---
+++ tests/2MultipleEnv.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+
+test.only("Multipl Env", async({page})=>
+{
+    console.log(process.env.URL)
+    console.log(process.env.USERNAME1)
+   // const url =process.env.URL
+    //await page.goto(url)
+})
