b3ae70ede9c966d6c8bf1cd7b40beb0a4f93918a
Zeeshan <mohdzee1414@gmail.com>
2026-09-11 01:02:37 +0530

    git-pr-frames

--- tests/16Frames.spec.js
+++ tests/16Frames.spec.js
@@ -11,12 +11,13 @@ test("Alert Popups", async({page})=>
       await  frames.getByPlaceholder("type in iframe").fill("Zeeshan")
       await frames.getByRole('button',{name :'Submit'}).click()
       await page.pause();
+      console.log("Frames setup")
       locator('[data-testid="practice-iframe"]').contentFrame().getByRole('button', { name: 'Submit' })
 
 //Nested frames
 await page.goto("https://demo.automationtesting.in/Frames.html#google_vignette"); 
       const f1 = await page.frameLocator("[src='MultipleFrames.html']").frameLocator("[src='SingleFrame.html']")
       await f1.locator("[type='text']").fill("Shan");
-      await expect(f1.locator("[type='text']")).toHaveText|("Shan")
+      await expect(f1.locator("[type='text']")).toHaveText("Shan")
 
 })
